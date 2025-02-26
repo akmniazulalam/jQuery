c724bdee8d521e695cf6fa37625dcb1e7d991597
niazulalam <niazulalam097@gmail.com>
2025-02-26 23:27:11 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,96 @@
+$(function(){
+    $("#hide").click(function(){
+        $(".box").hide(1000)
+    })
+    $("#show").click(function(){
+        $(".box").show(1000)
+    })
+    $("#showHide").click(function(){
+        $(".box").toggle(1000)
+    })
+    $("#fadeOut").click(function(){
+        $(".box").fadeOut(1000)
+    })
+    $("#fadeIn").click(function(){
+        $(".box").fadeIn(1000)
+    })
+    $("#fadeInOut").click(function(){
+        $(".box").fadeToggle(1000)
+    })
+    $("#slideUp").click(function(){
+        $(".box").slideUp(1000)
+    })
+    $("#slideDown").click(function(){
+        $(".box").slideDown(1000)
+    })
+    $("#slideUpDown").click(function(){
+        $(".box").slideToggle(1000)
+    })
+    $("#addNewBox").click(function(){
+        $(".box").addClass("box_tree")
+    })
+    $("#removeNewBox").click(function(){
+        $(".box").removeClass("box_tree")
+    })
+    $("#addRemoveBox").click(function(){
+        $(".box").toggleClass("box_tree")
+    })
+
+
+
+    
+})
+
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 2000
+  });
+  $('.counter').addClass('animated fadeInDownBig');
+  $('p').addClass('animated fadeIn');
+
+
+
+  (function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    //I'm adding this section so I don't have to keep updating this pen every year :-)
+    //remove this if you don't need it
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "03/03/",
+        birthday = dayMonth + yyyy;
+    
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+      birthday = dayMonth + nextYear;
+    }
+    //end
+    
+    const countDown = new Date(birthday).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          if (distance < 0) {
+            document.getElementById("headline").innerText = "It's my birthday!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+          }
+          //seconds
+        }, 0)
+    }());
